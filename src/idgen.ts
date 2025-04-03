e4dcdb0b43a4b6a083673132f236a1749afc908f
Sean Marquez <capsulecorplab@gmail.com>
2025-04-03 21:04:48 +0000

    tdd implement uniformints

--- src/idgen.ts
+++ src/idgen.ts
@@ -1,8 +1,41 @@
 import prand from "pure-rand";
 
 // Seed is the sum of the ASCII decimal codes for the string "OSHW"
-let rng = prand.xorshift128plus(79 + 83 + 72 + 87);
+const seed = 79 + 83 + 72 + 87;
+let rng = prand.xorshift128plus(seed);
 
-export function uniformint(a: number, b: number) {
-  return prand.unsafeUniformIntDistribution(a, b, rng);
+/**
+ * Returns a set of uniformly distributed integers between lBound and uBound, inclusive
+ *
+ * @param lBound inclusive lower bound of random selection
+ * @param uBound inclusive upper bound of random selection
+ * @param numInts number of uniformly distributed integers to return
+ * @returns an empty array if lBound or uBound are not integers or numInts is less than 1, else returns an array of randomly selected integers
+ */
+export function uniformints(
+  lBound: number,
+  uBound: number,
+  numInts: number
+): number[] {
+  let result: number[] = [];
+  if (
+    Number.isInteger(lBound) &&
+    Number.isInteger(uBound) &&
+    Number.isInteger(numInts) &&
+    uBound > lBound &&
+    numInts >= 1
+  ) {
+    for (let i = 0; i < numInts; i++) {
+      result.push(prand.unsafeUniformIntDistribution(lBound, uBound, rng));
+    }
+  }
+
+  return result;
+}
+
+/**
+ * Reset RNG for generating ID's, by recalling constructor w/ library seed
+ */
+export function resetRNG() {
+  rng = prand.xorshift128plus(seed);
 }
--- test/idgen.spec.ts
+++ test/idgen.spec.ts
@@ -1,1 +1,34 @@
-import { uniformint } from "../src/idgen";
+import { uniformints, resetRNG } from "../src/idgen";
+import { expect } from "chai";
+
+describe("Test Uniform Ints", () => {
+  it("should return an array, [5], if lBound == 1, uBound == 6, and numInts == 1", () => {
+    resetRNG();
+    const result = uniformints(1, 6, 1);
+    expect(result.toString()).to.equal([5].toString());
+  });
+  it("should return an empty array if lBound is not an integer", () => {
+    const result = uniformints(3.14, 5, 1);
+    expect(result.length).to.equal(0);
+  });
+  it("should return an empty array if uBound is not an integer", () => {
+    const result = uniformints(1, 3.14, 1);
+    expect(result.length).to.equal(0);
+  });
+  it("should return an empty array if numInts is not an integer", () => {
+    const result = uniformints(1, 3, 3.14);
+    expect(result.length).to.equal(0);
+  });
+  it("should return an empty array if numInts is less than 1", () => {
+    const result = uniformints(1, 3, 0);
+    expect(result.length).to.equal(0);
+  });
+  it("should return an empty array if lBound and uBound are not integers", () => {
+    const result = uniformints(3.14, 6.28, 1);
+    expect(result.length).to.equal(0);
+  });
+  it("should return an empty array if uBound is less than lBound", () => {
+    const result = uniformints(6, 3, 1);
+    expect(result.length).to.equal(0);
+  });
+});
