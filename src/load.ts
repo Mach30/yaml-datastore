338e4fcea3299281644c75a105a3526c24a627bc
Sean Marquez <capsulecorplab@gmail.com>
2025-06-29 15:54:27 +0000

    update load() implementation for nested objects of simple data types
    - update docstrings params for load() function
      - update workingDirectoryPath to include relative paths
      - update elementPath to include key-value pairs

--- src/load.ts
+++ src/load.ts
@@ -42,10 +42,24 @@ export class LoadResult {
 }
 
 /**
+ * Returns a relative directory path for a given elementPath
+ *
+ * @param elementPath object path (dot separated, with support for bracketed indexing for list elements or key-value pairs in objects) from working directory to element to be read into memory (e.g., top-element.sub-element.property[3])
+ */
+function toElementDirPath(elementPath: string): string {
+  const elementPathAsArray = elementPath.split(".");
+  if (elementPathAsArray.length > 1) {
+    return elementPath.split(".").slice(0, -1).join("/");
+  } else {
+    return elementPath;
+  }
+}
+
+/**
  * Returns a in-memory representation of the element in working directory specified by element path
  *
- * @param workingDirectoryPath absolute file path to working directory containing yaml-datastore serialized content
- * @param elementPath object path (dot separated, with support for bracketed indexing for list elements) from working directory to element to be read into memory (e.g., top-element.sub-element.property[3])
+ * @param workingDirectoryPath relative or absolute file path to working directory containing yaml-datastore serialized content
+ * @param elementPath object path (dot separated, with support for bracketed indexing for list elements or key-value pairs in objects) from working directory to element to be read into memory (e.g., top-element.sub-element.property[3])
  * @param depth integer from -1 to depth of element indicating how deep into element's hierachy to read into memory (-1 = read full depth. Defaults to -1)
  * @returns a LoadResult containing the status and content of the load function
  */
@@ -55,16 +69,49 @@ export function load(
   depth: number = -1
 ): LoadResult {
   const lsWorkingDirectoryPath = shell.ls(workingDirectoryPath).stdout;
+  const elementDirPath = toElementDirPath(elementPath);
   if (
     workingDirectoryPath != "" &&
-    lsWorkingDirectoryPath.includes(elementPath) &&
+    lsWorkingDirectoryPath.includes(elementDirPath) &&
     depth == -1
   ) {
-    const relativeElementPath = path.join(workingDirectoryPath, elementPath);
-    const lsElementPath = shell.ls(relativeElementPath).stdout;
-    if (lsElementPath.includes("_this.yaml")) {
-      const thisYamlPath = path.join(relativeElementPath, "_this.yaml");
-      const thisYaml = yaml.load(shell.cat(thisYamlPath).stdout);
+    const fullElementDirPath = path.join(workingDirectoryPath, elementDirPath);
+    const lsfullElementDirPath = shell.ls(fullElementDirPath).stdout;
+    if (lsfullElementDirPath.includes("_this.yaml")) {
+      const thisYamlFullPath = path.join(fullElementDirPath, "_this.yaml");
+      let thisYaml = yaml.load(shell.cat(thisYamlFullPath).stdout);
+      // iterate through elements of thisYaml and replace ((filepath)) with its associated complex data type
+      Object.entries(thisYaml).forEach(([key, value]) => {
+        const regex = /\(\(.*\)\)/;
+        const re = new RegExp(regex);
+        if (typeof value == "string") {
+          if (re.test(value.toString())) {
+            // parse filepath from ((filepath))
+            const aComplexDataTypeFilePath = value
+              .match(regex)[0]
+              .split("((")[1]
+              .split("))")[0];
+            const fullElementDirPath = path.join(
+              workingDirectoryPath,
+              elementDirPath,
+              aComplexDataTypeFilePath
+            );
+            const newWorkingDirectoryPath = fullElementDirPath
+              .split("/")
+              .slice(0, -2)
+              .join("/");
+            const newElementPath = fullElementDirPath
+              .split("/")
+              .slice(-2, -1)
+              .join("/");
+            thisYaml[key] = load(
+              newWorkingDirectoryPath,
+              newElementPath
+            ).element;
+          }
+        }
+      });
+      //console.log(thisYaml);
       return new LoadResult(true, thisYaml, elementPath);
     }
   }
