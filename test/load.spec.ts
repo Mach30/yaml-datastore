c84e4ec476a840a054c0fa97c43e4f56b1bf1b49
J. Simmons <simmons@j-dot.org>
2025-08-07 21:00:21 +0000

    Implement should load list for depth = 1

--- test/load.spec.ts
+++ test/load.spec.ts
@@ -690,7 +690,16 @@ describe("Test load function support for depth", () => {
     expect(toJsonString(result.element)).to.equal(toJsonString(expectedModel));
   });
   it("should load list for depth = 1", () => {
-    // TODO
+    const workingDir = "test/spec/2.2.3_list_of_objects_of_complex_data_types";
+    const elementPath = "model";
+    const depth = 1;
+    const expectedModel = JSON.parse(
+      fs.readFileSync(path.resolve(workingDir, "model_depth_1.json"), "utf8")
+    );
+    const result = load(workingDir, elementPath, depth);
+    expect(result.success).to.equal(true);
+    expect(result.message).to.equal(elementPath);
+    expect(toJsonString(result.element)).to.equal(toJsonString(expectedModel));
   });
   it("should load list for depth = 2", () => {
     // TODO
