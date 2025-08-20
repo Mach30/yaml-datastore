21da799f7c513e0e3b8b6f8bd914d6ac31e5e112
Sean Marquez <capsulecorplab@gmail.com>
2025-08-20 23:41:40 +0000

    Implement error cases for bad workingDirectoryPath

--- src/store.ts
+++ src/store.ts
@@ -2,11 +2,36 @@ import path from "path";
 import fs from "node:fs";
 import yaml from "js-yaml";
 
+export const INVALID_PATH_ERROR = "Error: Invalid path";
+export const NONEMPTY_WORKINGDIR_PATH_ERROR =
+  "Error: Working directory path is non-empty";
+
 /**
  * Represents results of a call to the store function
  */
 export class StoreResult {
-  //TODO
+  private _success: boolean;
+  private _message: string;
+
+  /**
+   * Default constructor for StoreResult
+   *
+   * @param success success status of store() operation
+   * @param message message describing success status of store() operation
+   * @returns new StoreResult object
+   */
+  constructor(success: boolean, message: string) {
+    this._success = success;
+    this._message = message;
+  }
+  /** @returns success status. */
+  public get success() {
+    return this._success;
+  }
+  /** @returns (TBD) on success or an explanation of the failure. */
+  public get message() {
+    return this._message;
+  }
 }
 
 /**
@@ -22,5 +47,18 @@ export function store(
   workingDirectoryPath: string,
   elementName: string
 ): StoreResult {
-  return new StoreResult();
+  if (fs.existsSync(workingDirectoryPath)) {
+    if (fs.readdirSync(workingDirectoryPath).length > 0) {
+      return new StoreResult(
+        false,
+        NONEMPTY_WORKINGDIR_PATH_ERROR + " [" + workingDirectoryPath + "]"
+      );
+    } else {
+      //TODO
+    }
+  }
+  return new StoreResult(
+    false,
+    INVALID_PATH_ERROR + " [" + workingDirectoryPath + "]"
+  );
 }
