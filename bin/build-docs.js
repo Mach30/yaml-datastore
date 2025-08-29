620e6532703b5578349f3cea022106662610c1fb
goldfishlaser <goldfishlaser@protonmail.com>
2025-08-29 01:39:56 +0000

    Implement generation of the spec dir README.md files

--- bin/build-docs.js
+++ bin/build-docs.js
@@ -3,7 +3,6 @@
 import sh from "shelljs";
 import path from "path";
 
-// 1.2. generate all of the spec dir README.md files
 // 2.0. one time generations
 // 2.1. run typedoc
 // 2.2. generate project README.md that includes everything
@@ -16,6 +15,13 @@ function generateModelTreeFile(specDirPath) {
   sh.cmd("tree", "model", "--noreport", "-o", TREE_FILE_NAME);
 }
 
+function generateReadmeFile(specDirPath) {
+  sh.cd(specDirPath);
+  if (sh.ls("-A").stdout.includes(".readme.md")) {
+    sh.cmd("markedpp", ".readme.md").to("README.md");
+  }
+}
+
 // 1.0. for each spec dir
 // get list of spec directories
 let specDirs = sh.ls(SPEC_DIR);
@@ -23,6 +29,9 @@ specDirs.pop(); // remove last entry since it is `spec.md`
 const projectDirPath = sh.pwd().toString();
 
 specDirs.forEach((specDirPath) => {
+  ", " > ", ";
   // 1.1. generate all of the spec dir tree files
   generateModelTreeFile(path.join(projectDirPath, SPEC_DIR, specDirPath));
+  // 1.2. generate all of the spec dir README.md files
+  generateReadmeFile(path.join(projectDirPath, SPEC_DIR, specDirPath));
 });
