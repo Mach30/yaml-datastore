a54bf2d4ef57b7c0aba037a131ca1f0a707a01e6
Sean Marquez <capsulecorplab@gmail.com>
2026-08-01 22:01:34 +0000

    set dirPath to "." if path.parse(filePath).dir is an empty string otherwise set to path.parse(filePath).dir

--- src/load.ts
+++ src/load.ts
@@ -33,7 +33,8 @@ function loadYaml(
       if (typeof (yamlAsJsObj as any)[key] == "string") {
         if (doubleParenthesesRegEx.test((yamlAsJsObj as any)[key])) {
           // parse filepath from ((filepath))
-          const dirPath = path.parse(filePath).dir;
+          const dirPath =
+            path.parse(filePath).dir === "" ? "." : path.parse(filePath).dir;
           const aComplexDataTypeFilePath = trimDoubleParentheses(
             (yamlAsJsObj as any)[key]
           );
